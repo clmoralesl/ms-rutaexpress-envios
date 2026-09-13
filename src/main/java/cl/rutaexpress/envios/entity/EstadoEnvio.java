72facc2af8d2f0844bd990e34b5b3e22565b4460
ValeskaSK <va.guardia@duocuc.cl>
2026-09-13 18:23:52 -0300

    feat(envios): inicializa microservicio y persistencia Oracle (#4)

---
+++ src/main/java/cl/rutaexpress/envios/entity/EstadoEnvio.java
@@ -0,0 +1,10 @@
+package cl.rutaexpress.envios.entity;
+
+public enum EstadoEnvio {
+    CREADO,
+    ACEPTADO,
+    EN_BODEGA,
+    EN_RUTA,
+    ENTREGADO,
+    CANCELADO
+}
---
+++ src/test/java/cl/rutaexpress/envios/MsRutaexpressEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.rutaexpress.envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRutaexpressEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
